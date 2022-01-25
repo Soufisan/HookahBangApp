47a3cad97e8f50f9268d565763321707124ce5f1
SoufianY <soufian.younoussi@gmail.com>
2022-01-25 20:44:20 +0100

    Arreglo en responsive de descripción

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,18 +1,12 @@
 import CardSabor from "../components/CardSabor";
 
 const CardList = ({ setSaborDescription, saborText }) => {
-
- 
-  
-
   return (
     <>
       <h3>Sabores de tu SHISHA</h3>
 
       <div className="general">
-        <CardSabor
-         
-        />
+        <CardSabor />
       </div>
     </>
   );
